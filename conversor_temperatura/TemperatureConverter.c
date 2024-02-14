21fa2ae025b39b83821936a763250f4138e64bf8
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-02-14 19:47:57 -0300

    Exercicio conversor de temperatura

---
+++ conversor_temperatura/TemperatureConverter.c
@@ -0,0 +1,31 @@
+/*
+Program> Temperature Converter
+Purpose> Convert temperatures from Celsius to Fahrenheit and vice versa.
+Author> Bruno Angelo;
+Date> February/2024;
+Version> 1.0
+*/
+void convertCelsiusToFahrenheit(){
+    float fltCelsius, fltFahrenheit;
+    printf("Enter the value in Celsius to be converted to Fahrenheit: ");
+    scanf("%f", &fltCelsius);
+    fltFahrenheit = (9 * fltCelsius + 160) / 5;
+    printf("The value in Celsius that was inputted converted to Fahrenheit is %f", fltFahrenheit);
+}
+
+void convertFahrenheitToCelsius(){
+    float fltCelsius, fltFahrenheit;
+    printf("Enter the value in Fahrenheit to be converted to Celsius: ");
+    scanf("%f", fltFahrenheit);
+    fltCelsius = ((fltFahrenheit - 32) * 5) / 9;
+    printf("The value in Fahrenheit that was inputted converted to Celsius is %f", fltCelsius);
+}
+
+int main(int argc, char const *argv[])
+{
+    char charSelection;
+    printf("Enter C for Celsius or F for Fahrenheit: ");
+    scanf("%c", charSelection);
+    charSelection == 'C' ? convertCelsiusToFahrenheit() : convertFahrenheitToCelsius();
+    return 0;
+}
