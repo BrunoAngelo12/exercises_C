d19476017eeb199f5e83f0feac23d1ea48054dcb
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-03-16 18:55:08 -0300

    exercicio recursao concluido

---
+++ exercicio_recursao/ExercicioRecursao.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+double calcularFatorial(int intN_Fatorial){
+    double calculo;
+    printf("Chamada recursiva %d", intN_Fatorial);
+    if(intN_Fatorial == 1){
+        return 1;
+    }else{
+        calculo = intN_Fatorial * calcularFatorial(intN_Fatorial - 1);
+        printf("Valor retornado %.2f", calculo);
+        return calculo;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int intN_Fatorial;
+    double doubleResultadoFatorial;
+    printf("Digite o número que deseje calcular o fatorial: ");
+    scanf("%d", &intN_Fatorial);
+    doubleResultadoFatorial = calcularFatorial(intN_Fatorial);
+    printf("Fatorial de %d = %.1f", intN_Fatorial, doubleResultadoFatorial);
+    return 0;
+}
